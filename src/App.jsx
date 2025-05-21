46b2c2711375e8c6bceb06bbda4c032395ed9532
harby pranata <harby23si@mahasiswa.pcr.ac.id>
2025-05-21 15:41:24 +0700

    saya sudah selesai buat penjualan

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from 'react-router-dom'
 import MainLayout from './components/MainLayout'
 import Dashboard from './pages/Dashboard'
+import SalesManagement from './pages/SalesManagement'
+import ProductManagement from './pages/ProductManagement'
 
 function App() {
  
@@ -8,6 +10,8 @@ function App() {
       <Routes>
         <Route element ={<MainLayout/>}>
         <Route path="/" element={<Dashboard/>}/>
+        <Route path="/penjualan" element={<SalesManagement/>}/>
+        <Route path="/produk" element={<ProductManagement/>}/>
         </Route>
       </Routes>
   
