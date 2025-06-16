1e456475ff9774da761e03b072018ba007080b3f
harby <harby23si@mahasiswa.pcr.ac.id>
2025-06-16 22:55:09 +0700

    Done halaman profile, janji temu

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,8 @@
+import { BsFillPeopleFill } from "react-icons/bs"; 
+import { GiTooth } from "react-icons/gi"; 
+import { BiBookOpen } from "react-icons/bi"; 
+import { AiOutlineMail } from "react-icons/ai"; 
+import { MdBedroomBaby } from "react-icons/md"; 
 import {
   LayoutDashboard,
   Users,
@@ -7,12 +12,13 @@ import {
   Settings,
   LogIn,
   MessageCircle,
+  CalendarCheck,
+  Tag, // untuk promo
 } from 'lucide-react';
 import { Link, useLocation } from 'react-router-dom';
 import { FaTooth } from 'react-icons/fa';
-import { CalendarCheck } from 'lucide-react';
-import { Tag } from 'lucide-react'; // Untuk ikon Promo
 
+// Menu utama
 const menuItems = [
   { name: 'Dashboard', icon: <LayoutDashboard />, path: '/' },
   { name: 'Produk', icon: <Box />, path: '/produk' },
@@ -22,8 +28,14 @@ const menuItems = [
   { name: 'PromoList', icon: <Tag />, path: '/PromoList' },
   { name: 'JanjiTemu', icon: <CalendarCheck />, path: '/JanjiTemu' },
   { name: 'TestimoniPasien', icon: <MessageCircle />, path: '/TestimoniPasien' },
+  { name: 'Reservasi', icon: <BiBookOpen />, path: '/Reservasi' },
+  { name: 'Email', icon: <AiOutlineMail />, path: '/Email' },
+  { name: 'Fasilitas', icon: <MdBedroomBaby />, path: '/Fasilitas' },
+  { name: 'JenisPerawatan', icon:<GiTooth /> , path: '/JenisPerawatan' },
+  { name: 'Profile', icon:<BsFillPeopleFill />, path: '/Profile' },
 ];
 
+// Menu akun
 const accountItems = [
   { name: 'Pengaturan Akun', icon: <Settings />, path: '/akun' },
   { name: 'Sign In', icon: <LogIn />, path: '/signin' },
@@ -47,6 +59,7 @@ const Sidebar = () => {
         />
       </div>
 
+      {/* Menu Navigasi */}
       <nav className="space-y-1">
         {menuItems.map((item) => (
           <Link
@@ -64,8 +77,8 @@ const Sidebar = () => {
         ))}
       </nav>
 
+      {/* Menu Akun */}
       <div className="mt-8 text-xs font-semibold text-[#1D5B5C]">AKUN</div>
-
       <nav className="mt-2 space-y-1">
         {accountItems.map((item) => (
           <Link
